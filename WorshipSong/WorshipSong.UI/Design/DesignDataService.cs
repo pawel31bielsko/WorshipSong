57bce9544bc7aed13ad4664949320a3b67b0767b
pawel31bielsko@gmail.com <nk89vxy2>
2014-12-10 22:52:28 +0100

    commit

---
+++ WorshipSong/WorshipSong.UI/Design/DesignDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using WorshipSong.UI.Model;
+
+namespace WorshipSong.UI.Design
+{
+    public class DesignDataService : IDataService
+    {
+        public void GetData(Action<DataItem, Exception> callback)
+        {
+            // Use this to create design time data
+
+            var item = new DataItem("Lorem ipsum dolor sit amet enim.\n Etiam ullamcorper. Suspendisse a pellentesque dui, non felis.\n Maecenas malesuada elit lectus felis, malesuada ultricies.");
+            callback(item, null);
+        }
+    }
+}
---
+++ WorshipSong/WorshipSong.UI/Model/DataService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WorshipSong.UI.Model
+{
+    public class DataService : IDataService
+    {
+        public void GetData(Action<DataItem, Exception> callback)
+        {
+            // Use this to connect to the actual data service
+
+            var item = new DataItem("Lorem ipsum dolor sit amet enim.\n\n Etiam ullamcorper. Suspendisse a pellentesque dui, non felis.\n Maecenas malesuada elit lectus felis, malesuada ultricies.");
+            callback(item, null);
+        }
+    }
+}
